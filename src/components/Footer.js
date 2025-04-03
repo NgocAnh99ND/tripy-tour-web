34c2393ffdd6a942a90e36193a0bb01a30b359be
NGOCANH99 <146905911+NgocAnh1999ND@users.noreply.github.com>
2025-04-03 15:13:52 +0700

    make footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import { FaAngleDown, FaAngleUp } from "react-icons/fa";
 import "./Footer.css";
+import tuyendung from "../image/tuyendung.jpeg";
+import thodienmayxanh from "../image/thodienmayxanh.png";
+import logotopzone from "../image/topzone.png";
+import erablue from "../image/erablue.png";
+import dienmayxanh from "../image/dienmayxanh.png";
+import bachhoaxanh from "../image/bachhoaxanh.png";
+import avakid from "../image/ava.jpeg";
+import nhathuoc from "../image/nhathuoc.png";
+import Facebook from "../image/FB.png";
+import Youtube from "../image/YTB.png";
+import Zalo from "../image/ZL.png";
+import Protected from "../image/protect.50.jpg";
 
 function Footer() {
+  const [showMore, setShowMore] = useState(false);
+  const additionalInfo = [
+    "Chính sách đổi trả",
+    "Giao hàng & Thanh toán",
+    "Hướng dẫn mua online",
+    "Bán hàng doanh nghiệp",
+    "Phiếu mua hàng",
+    "In hóa đơn điện tử",
+    "Quy chế hoạt động",
+    "Chính sách xử lý dữ liệu cá nhân",
+    "Nội quy cửa hàng",
+    "Chất lượng phục vụ",
+    "Cảnh báo giả mạo",
+    "Chính sách khui hộp sản phẩm Apple",
+    "Hợp tác bán hàng",
+    "Mạng xã hội thegioididong.com",
+    "Xem bản mobile",
+  ];
+
   return (
     <div className="footer-container">
       <div className="support">
@@ -55,8 +87,27 @@ function Footer() {
           <li>
             <a href="#">Chính sách bảo hành</a>
           </li>
+
+          {showMore &&
+            additionalInfo.map((info, index) => (
+              <li key={index}>
+                <a href="#">{info}</a>
+              </li>
+            ))}
+
           <li>
-            <a href="#">Xem thêm</a>
+            <button
+              onClick={() => setShowMore(!showMore)}
+              className="toggle-btn"
+            >
+              {showMore ? "Thu vào" : "Xem thêm"}
+
+              {showMore ? (
+                <FaAngleUp className="less-icon" />
+              ) : (
+                <FaAngleDown className="more-icon" />
+              )}
+            </button>
           </li>
         </ul>
       </div>
@@ -64,28 +115,51 @@ function Footer() {
       <div className="websites">
         <h4>Website của tập đoàn</h4>
         <div className="website-links">
-          <a href="#">topzone</a>
-          <a href="#">Điện máy Xanh</a>
-          <a href="#">Bách hóa Xanh</a>
-          <a href="#">Nhà thuốc An Khang</a>
-          <a href="#">Việc làm</a>
-          <a href="#">Erablue Electronics</a>
-          <a href="#">Thợ Sửa Chữa Xanh</a>
+          <a href="https://www.topzone.vn/">
+            <img src={logotopzone} alt="Topzone" />
+          </a>
+          <a href="https://www.dienmayxanh.com/">
+            <img src={dienmayxanh} alt="Điện máy Xanh" />
+          </a>
+          <a href="https://www.bachhoaxanh.com/">
+            <img src={bachhoaxanh} alt="Bách hóa Xanh" />
+          </a>
+          <a href="https://www.nhathuocankhang.com/">
+            <img src={nhathuoc} alt="Nhà thuốc An Khang" />
+          </a>
+          <a href="https://www.avakids.com/">
+            <img src={avakid} alt="AVA Kid" />
+          </a>
+          <a href="https://vieclam.thegioididong.com/">
+            <img src={tuyendung} alt="Việc làm" />
+          </a>
+          <a href="https://www.erablue.id/">
+            <img src={erablue} alt="Erablue Electronics" />
+          </a>
+          <a href="https://www.thodienmayxanh.com">
+            <img src={thodienmayxanh} alt="Thợ Sửa Chữa Xanh" />
+          </a>
         </div>
       </div>
 
-      <div className="social-media">
-        <a href="#">Facebook</a>
-        <a href="#">Zalo TGDD</a>
-        <a href="#">YouTube</a>
-      </div>
-
-      <div className="footer-bottom">
-        <p>
-          © 2018. Công ty cổ phần Thế Giới Di Động. GPKD: 0303217354. Địa chỉ:
-          128 Trần Quang Khải, P.Tân Định, Q.1, TP.HCM.
-        </p>
-        <p>Email: hotrotmdt@thegioididong.com</p>
+      <div className="social">
+        <div className="social-media">
+          <a href="https://www.facebook.com/thegioididongcom">
+            <img src={Facebook} alt="Facebook" />
+            3886.8k Fan
+          </a>
+          <a href="https://www.youtube.com/user/TGDDVideoReviews">
+            <img src={Youtube} alt="Youtube" />
+            873k Đăng ký
+          </a>
+          <a href="https://zalo.me/936590283559458736">
+            <img src={Zalo} alt="Zalo" />
+            Zalo TGDD
+          </a>
+        </div>
+        <div className="protected-group">
+          <img src={Protected} alt="" />
+        </div>
       </div>
     </div>
   );
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -29,6 +29,17 @@ function Home() {
       <Social />
       <SearchTags />
       <Footer />
+      <div className="footer-bottom">
+        <p>
+          © 2018. Công ty cổ phần Thế Giới Di Động. GPDKKD: 0303217354 do sở KH
+          & ĐT TP.HCM cấp ngày 02/01/2007. GPMXH: 238/GP-BTTTT do Bộ Thông Tin
+          và Truyền Thông cấp ngày 04/06/2020. Địa chỉ: 128 Trần Quang Khải,
+          P.Tân Định, Q.1, TP.Hồ Chí Minh. Địa chỉ liên hệ và gửi chứng từ: Lô
+          T2-1.2, Đường D1, Đ. D1, P.Tân Phú, TP.Thủ Đức, TP.Hồ Chí Minh. Điện
+          thoại: 028 38125960. Email: cskh@thegioididong.com. Chịu trách nhiệm
+          nội dung: Huỳnh Văn Tốt. Email: hotrotmdt@thegioididong.com.
+        </p>
+      </div>
     </div>
   );
 }
